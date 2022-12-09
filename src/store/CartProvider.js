20f8bfc7c3da36b8c37c5eef22a6b33c547eebcb
nicolofabris <nicolo.fabriss@gmail.com>
2022-12-09 15:14:12 +0100

    database data + order form

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -34,6 +34,12 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+
+  if(action.type === 'CLEAR') {
+    return defaultCartState
+  }
+
+
   if (action.type === 'REMOVE') {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.id
@@ -72,11 +78,16 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: 'REMOVE', id: id });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: 'CLEAR'})
+  }
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler
   };
 
   return (
